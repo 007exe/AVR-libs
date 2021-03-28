dda9d486f5d599ac570d91c909006bb49b040266
007exe <AlekseiChip@yandex.ru>
2021-03-28 13:10:21 +0300

    Заготовка проекта

---
+++ main-project/main.c
@@ -0,0 +1,10 @@
+﻿#include "main.h"
+
+int main(void)
+{
+    
+    while (1)
+    {
+
+    }
+}
---
+++ main-project/main.h
@@ -0,0 +1,38 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define F_CPU 8000000L          // Частота контролера в герцах
+
+#include <avr/io.h>             // Подключение устройств ввода/вывода в зависимости от типа МК AVR
+
+/*
+// http://avr-libc.narod.ru/modules.html
+#include <alloca.h>             // Захват пространства в стеке
+#include <assert.h>             // Диагностика
+#include <ctype.h>              // Операции с символами
+#include <errno.h>              // Ошибки системы
+#include <inttypes.h>           // Преобразования целочисленных типов
+#include <math.h>               // Математика
+#include <setjmp.h>             // Нелокальный goto
+#include <stdint.h>             // Стандартные целочисленные типы
+#include <stdio.h>              // Стандартные средства ввода/вывода
+#include <stdlib.h>             // Общие утилиты
+#include <string.h>             // Строки
+#include <avr/boot.h>           // Утилиты Поддержки Загрузчика
+#include <avr/eeprom.h>         // Работа с EEPROM
+#include <avr/fuse.h>           // Поддержка Fuse
+#include <avr/interrupt.h>      // Прерывания
+#include <avr/pgmspace.h>       // Утилиты Пространства Программ
+#include <avr/power.h>          // Управление потреблением микроконтроллера
+#include <avr/sfr_defs.h>       // Работа с битами
+#include <avr/sleep.h>          // Управление питанием и Спящий Режим
+#include <avr/version.h>        // avr-libc макрос версий
+#include <avr/wdt.h>            // Работа со сторожевым таймером
+#include <util/atomic.h>        // Atomically и Non-Atomically выполнение блоков кода
+#include <util/crc16.h>         // CRC Вычисления
+#include <util/delay.h>         // Функции циклов задержки
+#include <util/delay_basic.h>   // Основные функции циклов задержки
+#include <util/parity.h>        // Генерация бита четности
+#include <util/twi.h>           // TWI (I2C) определения
+*/
+#endif
